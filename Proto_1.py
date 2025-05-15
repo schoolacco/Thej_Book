0e8f481f127ef0ede438deaee73181a03dc94920
schoolacco <127917983+schoolacco@users.noreply.github.com>
2025-05-15 10:04:40 +1000

    I have almost completed the prototype, the only remaining issue to fix is attempting to fix the size of the ttk frame, after which the program will be relatively complete, the 2nd prototype will simply need a change in aesthetics and an official method of transitioning, as well as a proper version of the questionaire, after which in the final iteration all information will be added with aesthetics. The last major issues remaining is the frame, the transitioning and the randomisation of the questions.

--- Proto_1.py
+++ Proto_1.py
@@ -1,6 +1,6 @@
 from tkinter import *
-from tkinter import ttk
 from PIL import Image, ImageTk
+from tkinter import ttk
 def insert(string, root, list):
   Label(root, text=string, bg='#300900', fg='#9a1d00').place(relx=0.075, rely=0.05)
   for item in list:
@@ -27,10 +27,11 @@ def Questionaire(root):
   b4.place(relx=0.15, rely=0)
   List = [b1,b2,b3,b4]
 def main(root):
-  global subroot
   root.destroy()
   root = Tk()
-  frame = ttk.Frame(root)
+  s = ttk.Style()
+  s.configure('TFrame', background="#300900")
+  frame = Frame(root)
   frame.pack(fill=BOTH, expand=TRUE)
   root.state('zoomed')
   root.configure(bg='#300900')
@@ -44,13 +45,7 @@ def main(root):
   root.wm_iconphoto(False, photo)
   canvas = Canvas(frame, bg="#300900", width=2000, height=2000, bd=0, cursor="circle")
   canvas.pack(fill=BOTH, expand=True)
-  scrollbar_y = ttk.Scrollbar(frame, orient="vertical", command=canvas.yview)
-  scrollbar_y.pack(side="right", fill="y")
-  scrollbar_x = ttk.Scrollbar(frame, orient="horizontal", command=canvas.xview)
-  scrollbar_x.pack(side="bottom", fill="x")
-  canvas.configure(yscrollcommand=scrollbar_y.set, xscrollcommand=scrollbar_x.set)
-  content = ttk.Frame(canvas)
-  canvas.create_window((0, 0), window=content)
+  content = ttk.Frame(canvas, width=4000, height=4000, style='TFrame')
   Label(content, text="Just remember... you did this to yourself - ", bg='#300900', fg='#9a1d00', font=('Public Sans',20)).place(relx=0.32, rely=0.03)
   text = Label(content, text="Dante", bg='#300900', fg='#9a1d00', font=('Edwardian Script ITC',20))
   text.place(relx=0.6, rely=0.035)
@@ -58,6 +53,20 @@ def main(root):
   new_size = (image.width // 2, image.height // 2)  # Example: Shrink by half
   image = image.resize(new_size)
   image = ImageTk.PhotoImage(image)
+  # Create a canvas object and a vertical scrollbar for scrolling it.
+  vscrollbar = ttk.Scrollbar(content, orient=VERTICAL)
+  vscrollbar.pack(fill=Y, side=RIGHT, expand=FALSE)
+  canvas = Canvas(content, bd=0, highlightthickness=0,
+                     yscrollcommand=vscrollbar.set)
+  canvas.pack(side=LEFT, fill=BOTH, expand=TRUE)
+  vscrollbar.config(command=canvas.yview)
+  # Reset the view
+  canvas.xview_moveto(0)
+  canvas.yview_moveto(0)
+  # Create a frame inside the canvas which will be scrolled with it.
+  #content.interior = interior = ttk.Frame(canvas, width=4000, height=4000)
+  canvas.create_window(0, 0, window=content,
+                                     anchor=NW)
   Button(content, image=image, padx=0.1, pady=0.1, command=lambda: Questionaire(root)).place(relx=0.1, rely=0.1)
   Message(content, bg='#300900', fg='#9a1d00', text='''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec urna lacinia, dignissim sapien eget, interdum nisi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce non euismod odio, sed vehicula velit. Mauris vitae nulla auctor, euismod nibh vel, ullamcorper ipsum. Fusce aliquet ipsum at est consectetur porta. Curabitur erat mauris, tempor vel pretium nec, molestie sit amet nibh. Aliquam scelerisque gravida diam id porttitor. Vestibulum sit amet sodales purus, sed tincidunt massa. Vestibulum imperdiet dapibus tellus et accumsan. Pellentesque congue nulla a dolor blandit congue sit amet nec sapien. Ut molestie nulla eget nibh fermentum volutpat. Cras libero elit, dapibus non felis vel, molestie porttitor arcu. Morbi pretium diam mauris, in lobortis nulla aliquam blandit. Donec tempor hendrerit libero ullamcorper sodales.
 In non lorem porttitor, euismod nulla quis, ultrices sem. Nunc volutpat elit quis urna porttitor gravida. Morbi auctor vestibulum ultricies. Etiam in quam tincidunt, hendrerit enim in, pretium nunc. Nulla imperdiet ex vel porta consectetur. Vestibulum aliquam libero ac nisl facilisis malesuada. Etiam a facilisis ligula.
@@ -70,7 +79,7 @@ In non lorem porttitor, euismod nulla quis, ultrices sem. Nunc volutpat elit qui
 Donec vitae pellentesque felis. Integer pharetra enim quis purus viverra, congue accumsan nisl molestie. Curabitur sed lacus nibh. Ut egestas urna et dui volutpat, in molestie massa congue. Nam at sem ante. Suspendisse potenti. Praesent congue, felis sit amet cursus viverra, elit tellus facilisis elit, sed hendrerit leo nibh vel neque. Fusce eu turpis ut neque porta ullamcorper sit amet eu tellus. Vivamus lorem odio, ultrices ut ante at, posuere placerat diam. Cras pulvinar lorem non odio mollis, id accumsan lacus ornare. In laoreet aliquet tortor nec bibendum. In vel ligula nibh. Vestibulum semper lorem sed magna mattis, volutpat accumsan nunc cursus. Suspendisse semper pharetra semper. Nulla gravida faucibus nisl, eu laoreet odio porta id.
 Sed consequat, velit ut finibus tincidunt, magna lorem congue nulla, vel elementum ex augue vel mauris. Vestibulum ornare mi enim. In volutpat odio lacus, vel iaculis tortor tincidunt nec. Sed hendrerit fermentum purus non tristique. Sed feugiat scelerisque finibus. Fusce porta tristique mattis. Phasellus ullamcorper nisi at vehicula auctor. Cras maximus ultrices purus, et mollis enim finibus id. Aenean gravida, orci ut ultrices dictum, purus diam elementum dolor, id hendrerit lorem felis vel dolor. Sed aliquam tellus urna, et lacinia dui pretium luctus. Nulla pharetra est nunc, vitae consequat orci faucibus sit amet. Vivamus in orci et ligula molestie ornare vel vel enim. Aliquam nisl lectus, condimentum id ultrices eget, ullamcorper a ante.
 Nam congue fermentum elit ut interdum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nullam ac eros suscipit, interdum enim nec, facilisis massa. Proin iaculis magna sed nisi fermentum rhoncus. Quisque pulvinar sem quam, ut sollicitudin dolor hendrerit non. Donec non pellentesque ante. Morbi vel feugiat massa. Quisque vestibulum tortor ut risus malesuada, non posuere dui posuere. Nunc molestie sed ipsum at dignissim. Donec vel lobortis arcu. Integer eget tellus in quam dictum volutpat. Donec ullamcorper, turpis luctus aliquam sodales, felis nunc rhoncus turpis, sit amet accumsan tellus magna rhoncus purus. Quisque sit amet convallis lectus, ut sodales erat. Nullam commodo urna lectus, nec dictum libero dictum non. Aliquam erat volutpat.''').place(relx=0.1, rely=0.6)
-  content.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+  canvas.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
   content.pack()
   root.mainloop()
 root = Tk()
