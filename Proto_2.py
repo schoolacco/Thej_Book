8a294da064c7ac66bf26c54c901f2651358de30b
schoolacco <anthony.revel510@education.nsw.gov.au>
2025-05-20 17:33:41 +1000

    Update Proto_2.py
    Finished the aesthetics for each and every layer, Sprint 2 is complete, actual content of each layer (removing placeholders) shall be finally completed in sprint 2

--- Proto_2.py
+++ Proto_2.py
@@ -26,7 +26,8 @@ def Questionaire(root, order, answers, bg):
   b4 = Button(subroot, text=answers[3], command=lambda: insert(order[3], subroot, List))
   b4.place(relx=0.15, rely=0)
   List = [b1,b2,b3,b4]
-def layer(root, images, texts, command, bg, fg, intro_text, orders, answers):
+def layer(images, texts, command, bg, fg, intro_text, orders, answers):
+    global root
     root.destroy()
     root = Tk()
     root.state('zoomed')
@@ -56,16 +57,17 @@ def layer(root, images, texts, command, bg, fg, intro_text, orders, answers):
     content.bind("<Configure>", update_scrollregion)
 
     # Content starts here
-    Label(content, text=intro_text, 
-          bg=bg, fg=fg, font=('Public Sans', 20)).pack(pady=10)
+    Message(content, text=intro_text, width=800, bg=bg, fg=fg, font=('Public Sans', 20)).pack(pady=10)
+    image_ref = []
     for i in range(len(images)):
     # Load images
-      image = Image.open(images[i-1])
+      image = Image.open(images[i])
       image = image.resize((image.width // 2, image.height // 2))
       img_tk = ImageTk.PhotoImage(image)
-      Button(content, image=img_tk, padx=1, pady=1, command=lambda: Questionaire(root, orders[i-1], answers[i-1], bg)).pack(pady=10)
-      Message(content, bg=bg, fg=fg, width=800, text=texts[i-1]).pack(pady=10)
-      Button(content, text="Descend deeper", bg=bg, fg=fg, command=command)
+      image_ref.append(img_tk)
+      Button(content, image=img_tk, padx=1, pady=1, command=lambda: Questionaire(root, orders[i], answers[i], bg)).pack(pady=10)
+      Message(content, bg=bg, fg=fg, width=800, text=texts[i]).pack(pady=10)
+    Button(content, text="Descend deeper", bg=bg, fg=fg, command=command).pack()
     root.mainloop()
 
 root = Tk()
@@ -82,9 +84,19 @@ canvas = Canvas(root, bg="#300900", width=500, height=500, bd=0, cursor="circle"
 canvas.pack(fill=BOTH, expand=True)
 Label(canvas, text="Welcome to the world of Dante's Inferno" ,bg='#300900', fg='#9a1d00', font=('Terminal', 50)).pack()
 Label(canvas, text="\n \n \n \n \n \n \n \n",bg='#300900', fg='#9a1d00').pack()
-Button(canvas, text="Join the souls of the damned", bg='#300900', fg='#9a1d00' ,font=('Symbol', 15), command=lambda: layer(root, ["placeholder.png", "placeholder.png"], ["lorem ipsum", "lorem ipsum"], bg="#300900", fg='#9a1d00', intro_text="Just remember... you did this to yourself", 
-                                                                                                                           command=layer(root, ["placeholder.png", "placeholder.png"], ["lorem ipsum", "lorem ipsum"], bg="#300900", fg='#9a1d00', intro_text="Just remember... you did this to yourself", 
-                                                                                                                                         command=None))).pack()
+Button(canvas, text="Join the souls of the damned", bg='#300900', fg='#9a1d00' ,font=('Symbol', 15), command=lambda: layer(["placeholder.png", "placeholder.png"], ["lorem ipsum", "lorem ipsum"], bg="#300900", fg='#9a1d00', intro_text="Just remember... you did this to yourself, Welcome to the threshold of Hell, here you'll find the apathetic.", orders=["Incorrect", "Incorrect", "Correct", "Incorrect"], answers=["Answer1", "Answer2", "Answer3", "Answer4"], 
+                                                                                                                           command=lambda: layer(["placeholder.png", "placeholder.png"], ["lorem ipsum", "lorem ipsum"], bg="#251317", fg='#FFFFFF', intro_text="This is Limbo, the first circle of Hell, here you will find pagans and the unbaptized.", orders=["Incorrect","Correct", "Incorrect", "Incorrect"], answers=["Answer1", "Answer2", "Answer3", "Answer4"], 
+                                                                                                                            command=lambda: layer(["placeholder.png", "placeholder.png"], ["lorem ipsum", "lorem ipsum"], bg="#1B1D2A", fg='#FFFFFF', intro_text="This is the 2nd circle of Hell, here the lustful are punished", orders=["Incorrect","Correct", "Incorrect", "Incorrect"], answers=["Answer1", "Answer2", "Answer3", "Answer4"],
+                                                                                                                             command=lambda: layer(["placeholder.png", "placeholder.png"], ["lorem ipsum", "lorem ipsum"], bg="#745E3F", fg='#FFFFFF', intro_text="This is 3rd circle of Hell, here the gluttonous are punished", orders=["Incorrect","Correct", "Incorrect", "Incorrect"], answers=["Answer1", "Answer2", "Answer3", "Answer4"], 
+                                                                                                                              command=lambda: layer(["placeholder.png", "placeholder.png"], ["lorem ipsum", "lorem ipsum"], bg="#98754B", fg='#FFFFFF', intro_text="This is 4th circle of Hell, here the avarcious and the spendthrifts are punished", orders=["Incorrect","Correct", "Incorrect", "Incorrect"], answers=["Answer1", "Answer2", "Answer3", "Answer4"], 
+                                                                                                                               command=lambda: layer(["placeholder.png", "placeholder.png"], ["lorem ipsum", "lorem ipsum"], bg="#4D4742", fg='#FFFFFF', intro_text="This is 5th circle of Hell, here the wrathful and the melancholic are punished", orders=["Incorrect","Correct", "Incorrect", "Incorrect"], answers=["Answer1", "Answer2", "Answer3", "Answer4"], 
+                                                                                                                                command=lambda: layer(["placeholder.png", "placeholder.png"], ["lorem ipsum", "lorem ipsum"], bg="#494949", fg='#FFFFFF', intro_text="This is 6th circle of Hell, here lies the heretics", orders=["Incorrect","Correct", "Incorrect", "Incorrect"], answers=["Answer1", "Answer2", "Answer3", "Answer4"], 
+                                                                                                                                 command=lambda: layer(["placeholder.png", "placeholder.png"], ["lorem ipsum", "lorem ipsum"], bg="#2B0A05", fg='#FFFFFF', intro_text="Welcome, you have come far travellar, this is the 1st ring of Hell of the 7th circle, the layer of Malice, containing the punishments of those whose who have commited sins of violence and inhumanity. The first ring contains those who bring violence upon others or their property.", orders=["Incorrect","Correct", "Incorrect", "Incorrect"], answers=["Answer1", "Answer2", "Answer3", "Answer4"], 
+                                                                                                                                  command=lambda: layer(["placeholder.png", "placeholder.png"], ["lorem ipsum", "lorem ipsum"], bg="#707070", fg='#FFFFFF', intro_text="This is the 2nd ring of the 7th circle, here lies those who have commited violence against themself or their property. You cannot take the easy way to Paradise.", orders=["Incorrect","Correct", "Incorrect", "Incorrect"], answers=["Answer1", "Answer2", "Answer3", "Answer4"], 
+                                                                                                                                   command=lambda: layer(["placeholder.png", "placeholder.png"], ["lorem ipsum", "lorem ipsum"], bg="#938676", fg='#FFFFFF', intro_text="The 3rd and last ring of the 7th circle of Hell, here you find those who dared to hurt God and Nature, they deserved no lesser of a fate.", orders=["Incorrect","Correct", "Incorrect", "Incorrect"], answers=["Answer1", "Answer2", "Answer3", "Answer4"], 
+                                                                                                                                    command=lambda: layer(["placeholder.png", "placeholder.png"], ["lorem ipsum", "lorem ipsum"], bg="#826A3C", fg='#FFFFFF', intro_text="You have ventured very far traveller, to the 8th circle of Hell, here you shall find those who have commited sins of deceit against those who have no cause of trust. The pimps and seducers, the flatterers, the simonists, the soothsayers, the corrupt officials, the hypocrites, the theieves, the intellectually dishonest, the rabble-rousers and the counterfeiters, all in their own smaller boliga (all in one place cause I'm lazy).", orders=["Incorrect","Correct", "Incorrect", "Incorrect"], answers=["Answer1", "Answer2", "Answer3", "Answer4"], 
+                                                                                                                                     command=lambda: layer(["placeholder.png", "placeholder.png"], ["lorem ipsum", "lorem ipsum"], bg="#3C4FB2", fg='#FFFFFF', intro_text="Your journey approaches its end traveller, this is the 9th and final circle of Hell, containing all of those who have commited sins of deceit against those who have cause to trust, those who have dared to decieve their family, their nation, benefactors and have dared to defy xenia, you approach the centre, if you truly wish to see those who have commited the most foul sins.", orders=["Incorrect","Correct", "Incorrect", "Incorrect"], answers=["Answer1", "Answer2", "Answer3", "Answer4"], 
+                                                                                                                                      command=lambda: layer(["placeholder.png", "placeholder.png"], ["lorem ipsum", "lorem ipsum"], bg="#300900", fg='#9a1d00', intro_text="...", orders=["Incorrect","Correct", "Incorrect", "Incorrect"], answers=["Answer1", "Answer2", "Answer3", "Answer4"], command=lambda: root.destroy() )))))))))))))).pack()
 Label(canvas, text="LEAVE", bg='#300900', fg='#400900', font=('wingdings', 10)).pack()
 if __name__ == "__main__":
   root.mainloop()
